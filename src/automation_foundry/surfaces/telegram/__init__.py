c5a24aa4780788c8c3518c5ec6cbdb7339cb0e70
Rushendra <rushendra.s20@iiits.in>
2026-07-12 01:25:49 -0700

    feat: gate Telegram learning behind owner disclosure

--- src/automation_foundry/surfaces/telegram/__init__.py
+++ src/automation_foundry/surfaces/telegram/__init__.py
@@ -6,10 +6,30 @@ from automation_foundry.surfaces.telegram.callbacks import (
     TelegramCallbackStore,
     TelegramCallbackStoreConfig,
 )
+from automation_foundry.surfaces.telegram.interactions import (
+    PROVIDER_DISCLOSURE_SHA256,
+    PROVIDER_DISCLOSURE_TEXT,
+    TelegramButton,
+    TelegramChatType,
+    TelegramInboundUpdate,
+    TelegramInteractionService,
+    TelegramSurfaceConfig,
+    TelegramSurfaceResponse,
+    TelegramUpdateKind,
+)
 
 __all__ = [
     "IssuedTelegramCallback",
+    "PROVIDER_DISCLOSURE_SHA256",
+    "PROVIDER_DISCLOSURE_TEXT",
     "TelegramCallbackRejectedError",
     "TelegramCallbackStore",
     "TelegramCallbackStoreConfig",
+    "TelegramButton",
+    "TelegramChatType",
+    "TelegramInboundUpdate",
+    "TelegramInteractionService",
+    "TelegramSurfaceConfig",
+    "TelegramSurfaceResponse",
+    "TelegramUpdateKind",
 ]
